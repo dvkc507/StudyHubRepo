e4c51743a9686b4e8370d34875b4cd038860428b
vdoppalapudi <>
2023-08-12 18:01:50 +0530

    Revert "add last"
    This reverts commit 8ef62689aafdf52f29c61af162f5b84b93d3eb67.

--- WSIStudyHall/src/main/java/Prolifics/WSIStudyHall/App.java
+++ WSIStudyHall/src/main/java/Prolifics/WSIStudyHall/App.java
@@ -23,7 +23,11 @@ public class App
         System.out.println( "Hello World!" );
         // commit 1
         // commit 2
-        // commit 3s
-        //commit last
+        // commit 3
+<<<<<<< HEAD
+        // commit 4
+        // commit 6
+=======
+>>>>>>> parent of 22e048c... added commit 4 from eclipse
     }
 }
